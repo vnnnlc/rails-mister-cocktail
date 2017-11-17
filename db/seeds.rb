a7442e1d62cce5d1c19064506fb3f8fa632984de
vnnnlc <vnnnlc@gmail.com>
2017-11-17 17:52:20 +0100

    shitty website

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "open-uri"
-require "json"
+require 'rest-client'
+require 'json'
 
-Ingredient.destroy_all
+cocktail_rows = 100
+ingredients_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+cocktail_random_url = "http://www.thecocktaildb.com/api/json/v1/1/random.php"
 
-
-
-puts "Start seeding"
-
-url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-file_json = open(url).read
-json = JSON.parse(file_json)
-
-
-drinks = json["drinks"]
-
-puts "Creating ingredients"
-drinks.each do |ingredient|
-  Ingredient.create!(name: ingredient["strIngredient1"])
+def get_json(url)
+ JSON.parse(RestClient.get(url))
 end
 
-puts "Created #{Ingredient.count} ingredients"
+puts "Creating rows for Ingredients"
+get_json(ingredients_url)["drinks"].each do |ingredient|
+ Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts "Adding #{cocktail_rows} rows to Cocktails"
+cocktail_rows.times do
+ cocktail_hash = get_json(cocktail_random_url)["drinks"][0]
+ cocktail_obj = Cocktail.create(name: cocktail_hash["strDrink"], remote_photo_url: cocktail_hash["strDrinkThumb"])
+
+ 15.times do |n|
+   index_number = n + 1
+   ingredient_name = cocktail_hash["strIngredient#{index_number}"]
+
+   unless ingredient_name.nil? || ingredient_name.empty?
+     ingredient = Ingredient.where(name: ingredient_name).first
+   end
+ end
+end
